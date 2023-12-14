37c0e59c7408efe2b53d1b765b9e81cc1506cd9d
Dmitriy Sagalaev <dmitry-sagalaev2017@yandex.ru>
2023-12-14 14:35:53 +0500

    my project

---
+++ module_2_syntax/lessons/functions/src/solution_2.py
@@ -0,0 +1,8 @@
+worker_name1 = "Света"
+worker_name2 = "Маша"
+worker_name3 = "Олег"
+worker_name4 = "Паша"
+group = [worker_name1, worker_name2, worker_name3, worker_name4]
+print('В чётные дни работают:', group[::2])
+print()
+print('В нечётные дни работают:', group[1::2])
